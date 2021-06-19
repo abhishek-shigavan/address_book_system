fedc4d3c82bd57cefafe1e05b5a169289229381a
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-19 22:02:53 +0530

    Add: Welcome message added

---
+++ AddressBook.java
@@ -0,0 +1,14 @@
+/**
+ * AddressBook --- Program for simulation of Address Book System
+ * 
+ * 
+ * @author Abhishek Shigavan
+ *
+ */
+public class AddressBook {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Address Book Management System");
+	}
+}
