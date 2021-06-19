7072243652b413ff2d78856cbabca88efefb41be
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-19 22:45:49 +0530

    Add: Doing CRED Operations

--- ContactStore.java
+++ ContactStore.java
@@ -22,6 +22,58 @@ public class ContactStore {
 		contactList.add(contactDetails);
 	}
 /**
+ * This method takes first name as parameter
+ * and check weather it is present in ArrayList or not
+ * 	
+ * @param first_name
+ * 
+ * @return flag value 
+ * 			true -- if name is present
+ * 			false -- if name is not present
+ */
+	public boolean checkContact(String first_name) {
+		
+		boolean flag = false;
+		for(int i=0; i < contactList.size(); i++) {
+			
+			if(contactList.get(i).getFirst_Name().equals(first_name)) {
+				
+				flag = true;
+			}
+		}
+		
+		return flag;	
+	}
+/**
+ * This method matches first name with ArrayList
+ * element.
+ * And sets user given value for all the field associated
+ * with first name
+ *   	
+ * @param first_name
+ * @param contactDetails
+ * 
+ * @return No return
+ */
+	public void editContact(String first_name, ContactDetails contactDetails) {
+		
+		for(int i =0; i < contactList.size(); i++) {
+			
+			if(contactList.get(i).getFirst_Name().equals(first_name)) {
+				
+				contactList.get(i).setFirst_Name(contactDetails.getFirst_Name());
+				contactList.get(i).setLast_Name(contactDetails.getLast_Name());
+				contactList.get(i).setAddress(contactDetails.getAddress());
+				contactList.get(i).setCity(contactDetails.getCity());
+				contactList.get(i).setState(contactDetails.getState());
+				contactList.get(i).setZip_Code(contactDetails.getZip_Code());
+				contactList.get(i).setMob_No(contactDetails.getMob_No());
+				contactList.get(i).setEmail(contactDetails.getEmail());
+				
+			}
+		}		
+	}
+/**
  * This method prints all the contacts
  * of ArrayList
  * 
@@ -34,5 +86,6 @@ public class ContactStore {
 			System.out.println(contactList.get(i));
 		}
 	}
-
+	
 }
+
