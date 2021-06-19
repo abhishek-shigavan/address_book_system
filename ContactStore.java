15cbe867595a8c7a1ad42b8312f100c34900ca04
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-19 22:17:51 +0530

    Add: Doing CRED Operations

---
+++ ContactStore.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * ContactStore --- Program to store the Contact Details
+ * 
+ * @author Abhishek Shigavan
+ *
+ */
+public class ContactStore {
+	
+	//creating ArrayList
+	List<> contactList = new ArrayList();
+
+}
